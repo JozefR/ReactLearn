ffab87cddb54e89a581d4a3daedae255c42af871
Jozef Randjak <jozef.randjak@auftrag.at>
2022-07-04 10:00:29 +0200

    colocation slow

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// TIC TAC TOE GAME written using class components
 class Game extends React.Component {
   constructor(props) {
     super(props);
@@ -215,7 +218,56 @@ function calculateWinner(squares) {
   return null;
 }
 
-// ========================================
+//root.render(<Game />);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+// ============================================================================
+
+// COLOCATION Slow
+function sleep(time) {
+  const done = Date.now() + time;
+  while (done > Date.now()) {
+    // sleep...
+  }
+}
+
+// imagine that this slow component is actually slow because it's rendering a
+// lot of data (for example).
+function SlowComponent({ time, onChange }) {
+  sleep(time);
+  return (
+    <div>
+      Wow, that was{" "}
+      <input
+        value={time}
+        type="number"
+        onChange={(e) => onChange(Number(e.target.value))}
+      />
+      ms slow
+    </div>
+  );
+}
+
+function DogName({ time, dog, onChange }) {
+  return (
+    <div>
+      <label htmlFor="dog">Dog Name</label>
+      <br />
+      <input id="dog" value={dog} onChange={(e) => onChange(e.target.value)} />
+      <p>{dog ? `${dog}'s favorite number is ${time}.` : "enter a dog name"}</p>
+    </div>
+  );
+}
+
+function Colocation() {
+  // this is "global state"
+  const [dog, setDog] = React.useState("");
+  const [time, setTime] = React.useState(200);
+  return (
+    <div>
+      <DogName time={time} dog={dog} onChange={setDog} />
+      <SlowComponent time={time} onChange={setTime} />
+    </div>
+  );
+}
+
+root.render(<Colocation />);
