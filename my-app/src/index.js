8f7236298bff21e680e7d8baf981c0b9de223448
Jozef Randjak <randjakjozef@gmail.com>
2022-07-14 18:12:24 +0200

    missing merge

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,18 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-<<<<<<< Updated upstream
 import TicTacToeHooks from "./TicTacToeHooks";
 import TicTacToeClasses from "./TicTacToeClasses";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//root.render(<TicTacToeClasses />);
-root.render(<TicTacToeHooks />);
-=======
 import Reducer from "./reducer.js";
-import TicTacToeClasses from "./ticTacToeClasses";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<TicTacToeClasses />);
->>>>>>> Stashed changes
